15d4df9a5b4383e53b083a6f722f2633cde1a86c
Riddhi Gopanwar <riddhigopanwar@gmail.com>
2026-09-20 18:47:24 +0530

    Rename anon-key to SUPABASE_ANON_KEY

--- src/main/resources/static/js/supabase-config.js
+++ src/main/resources/static/js/supabase-config.js
@@ -6,5 +6,5 @@
    ========================================================================== */
 
 const SUPABASE_URL = 'https://qympmgmkmbrvnzlfyomu.supabase.co';
-const supabase.anon-key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bXBtZ21rbWJydm56bGZ5b211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4ODU5NTMsImV4cCI6MjEwNTQ2MTk1M30.AVHVjBL1dNB2DEdNib5P-67ixwiCONq7gy2mZJlMP7k
+const SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bXBtZ21rbWJydm56bGZ5b211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4ODU5NTMsImV4cCI6MjEwNTQ2MTk1M30.AVHVjBL1dNB2DEdNib5P-67ixwiCONq7gy2mZJlMP7k
 const supabaseClient = window.supabase.createClient(https://qympmgmkmbrvnzlfyomu.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5bXBtZ21rbWJydm56bGZ5b211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4ODU5NTMsImV4cCI6MjEwNTQ2MTk1M30.AVHVjBL1dNB2DEdNib5P-67ixwiCONq7gy2mZJlMP7k);
